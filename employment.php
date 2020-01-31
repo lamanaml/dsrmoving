869a3c68ee3fa49441499c5e454f1547b37e8646
LaMana Donadelle <36721091+lamanaml@users.noreply.github.com>
2020-01-31 14:15:49 -0500

    switched to php

--- employment.php
+++ employment.php
@@ -15,38 +15,39 @@
 </head>
 <body>
 
-  <!-- Navigation -->
-  <nav class="white" role="navigation">
-    <div class="nav-wrapper container">
-       <a id="logo-container" href="index.html" class="brand-logo"><img src="assets/images/DSRMovingLogo.636.png"></a>
-            <ul class="right hide-on-med-and-down">
+<!-- Navigation -->
+
+   <nav class="white" role="navigation">
+      <a id="logo-container" href="index.html" class="brand-logo "><img src="assets/images/logo.gif"></a>
+      <ul class="right hide-on-med-and-down">
       </ul>
       <ul class="right hide-on-med-and-down">
-        <li><a href="https://ant.hellomoving.com/DSRMOVING/index.htm">Quote Request</a></li>
+         <li><a href="https://ant.hellomoving.com/DSRMOVING/index.htm">Quote Request</a></li>
       </ul>
       <ul class="right hide-on-med-and-down">
-        <li><a href="contact.html">Contact</a></li>
-      </ul>
-        <ul class="right hide-on-med-and-down">
-        <li><a href="employment.html">Employment</a></li>
+         <li><a href="contact.html">Contact</a></li>
       </ul>
       <ul class="right hide-on-med-and-down">
-        <li><a href="#services">Services</a></li>
+         <li><a href="employment.html">Employment</a></li>
+         </ul>
+      <ul class="right hide-on-med-and-down">
+         <li><a href="services.html">Services</a></li>
       </ul>
-      <!-- mobile navigation -->
-    <a href="#" data-target="nav-mobile" class="sidenav-trigger"><i class="material-icons">menu</i></a>
+
+<!-- mobile navigation -->
+   <a href="#" data-target="nav-mobile" class="sidenav-trigger"><i class="material-icons">menu</i></a>
 
       <ul id="nav-mobile" class="sidenav">
-        <li><a href="#services">Services</a></li>
+         <li><a href="services.html">Services</a></li>
       </ul>
       <ul id="nav-mobile" class="sidenav">
-        <li><a href="contact.html">Contact</a></li>
+         <li><a href="contact.html">Contact</a></li>
       </ul>
       <ul id="nav-mobile" class="sidenav">
-        <li><a href="employment.html">Employment</a></li>
+         <li><a href="employment.html">Employment</a></li>
       </ul>
-    </div>
-  </nav>
+   </div>
+</nav>
 
 <!-- Hero -->
   <div id="index-banner" class="parallax-container">
@@ -122,14 +123,13 @@
 
 </div>
 
-
 <!-- footer -->
   <footer class="page-footer teal">
     <div class="container">
       <div class="row">
         <div class="col l6 s12">
-          <h5 class="white-text">Company Bio</h5>
-          <p class="grey-text text-lighten-4">We are a team of college students working on this project like it's our full time job. Any amount would help support and continue development on this project and is greatly appreciated.</p>
+          <h5 class="white-text">DSR Moving Professionals</h5>
+          <p class="grey-text text-lighten-4">We work closely with our clients to customize the services we provide based on their specific needs and circumstances. We are ready and willing to go the extra mile whenever and wherever we’re needed. Contact us today to get in touch with our team of experienced professionals.</p>
         </div>
         <div class="col l3 s12">
           <h5 class="white-text">Connect</h5>
@@ -137,21 +137,21 @@
             <li><a class="white-text" href="https://www.yelp.com/biz/dsr-moving-corporation-raleigh">Yelp</a></li>
             <li><a class="white-text" href="#!">Google</a></li>
             <li><a class="white-text" href="https://www.facebook.com/dsrmovingnc/">Facebook</a></li>
-            
           </ul>
         </div>
         <div class="col l3 s12">
           <h5 class="white-text">Find us</h5>
-          <ul>
-                       <li><a class="white-text" href="#!">251 Dominion Dr, Morrisville, NC 27560</a></li>
-            <li><a class="white-text" href="#!">1722 E. 11th Street, Siler City, NC 27344</a></li>
+          <ul >
+            <li >251 Dominion Dr, Morrisville, NC 27560</li>
+           
+            <li >1722 E. 11th Street, Siler City, NC 27344</li>
           </ul>
         </div>
       </div>
     </div>
     <div class="footer-copyright">
       <div class="container">
-    © 2005 by DSR Moving Corporation
+        © 2005 by DSR Moving Corporation
       </div>
     </div>
   </footer>
