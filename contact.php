fd52d01844dd55136396291ff9145beb7acd0e1f
LaMana Donadelle <36721091+lamanaml@users.noreply.github.com>
2020-02-23 19:31:10 -0500

    updated css

--- contact.php
+++ contact.php
@@ -24,28 +24,37 @@
   </div>
 <!-- <Main Content> -->
 <div class="container">
-<div class="row">
-      <div class="col l8 s12"><h2 class="header">Contact Info</h2></div>
-      <p>We move to and from The Triangle Area (Raleigh, Durham, Chapel Hill) and the Triad (Greensboro, Winston Salem, High Point) and surrounding areas.</p>
-      <p>Questions or comments about your upcoming move? We are happy to help!</p>
+  <div class="row">
+    <div class="col l8 s12">
+      <h2 class="header">How can we help you?</h2>
+     
 
-      <h5 >Call or email us</h5>
-      <p><a href="mailto:dsrmoving@gmail.com">dsrmoving@gmail.com</a></p>
-      <p >919-796-2673</p>
-<hr>
-      <h5 >Find us</h5>
-      <p >251 Dominion Dr, Morrisville, NC 27560</p>
-      <p >1722 E. 11th Street, Siler City, NC 27344</p>
- <hr>
-      <h5 >Frequently Asked Questions</h5>
-      Check out our <a href="movingguide.php">DSR Moving Guide</a>  to find answers to our most common questions!
- <hr>
-        <h5 >File a Claim</h5>   
-        <p>At DSR Moving, we pride ourselves in treating your move with caution and care.  In the event that something gets damaged or lost, we will resolve the issue in the a fair and timely manner</p>
-        <a href="claims.php">File a Claim</a>
-  </div>
-  </div>           
-           
+    <p>At DSR Moving, we pride ourselves in giving our customers the best possible experience at the best possible price!  If there is anything that we can do to help you, please let us know!
+    
+    <p>We move to and from The Triangle Area (Raleigh, Durham, Chapel Hill) and the Triad (Greensboro, Winston Salem, High Point) and surrounding areas.</p>
+    
+    <div>
+      <a href="https://ant.hellomoving.com/DSRMOVING/index.htm" id="download-button" class="btn-large waves-light teal lighten-1 ">Request FREE a Quote</a>
+    </div>
+
+    <h5 >Contact Number</h5>
+     <p >919-796-2673</p>
+
+    <h5 >Email us</h5>
+     <p><a href="mailto:dsrmoving@gmail.com">dsrmoving@gmail.com</a></p>
+    <hr>
+    <h5 >Locations</h5>
+    <p >251 Dominion Dr, Morrisville, NC 27560</p>
+    <p >1722 E. 11th Street, Siler City, NC 27344</p>
+ 
+    <h5 >Frequently Asked Questions</h5>
+    Check out our <a href="movingguide.php">DSR Moving Guide</a> to find answers to our most common questions!
+  <hr>
+    <h5 >File a Claim</h5>   
+    <p>At DSR Moving, we pride ourselves in treating your move with caution and care.  In the event that something gets damaged or lost, we will resolve the issue in the a fair and timely manner</p>
+    <a href="claims.php">File a Claim</a>
+    </div>
+    </div>           
 
 <!-- footer -->
 <?php include 'footer.html';?>
