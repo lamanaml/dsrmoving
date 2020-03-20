cd2a2cd4c71391eecfe6d547774a9bc9999f888a
LaMana Donadelle <36721091+lamanaml@users.noreply.github.com>
2020-03-20 19:25:44 -0400

    updated coronavirus

--- coronavirus.php
+++ coronavirus.php
@@ -27,27 +27,26 @@
   <div>
   We at DSR want to assure you we are putting the safety of our customers and team members first. We’re following all best practices and guidelines for sanitation, cleanliness, and precaution as we continue to help our customers move forward. If you have any questions regarding scheduling a move, a move you already have scheduled, or information surrounding COVID-19 Coronavirus and our business, please give us a call.  Below are a few common questions.
   </div>
-  <div><h4>Is my move still on?</h4></div>
+  <div><h5>Is my move still on?</h5></div>
 
   <div>Unless we call to cancel or postpone your move, it’s still business as usual for all of our customers who are booked. If you have any questions or concerns about your scheduled move, please give us a call.</div>
 
-  <div><h4>What precautions is DSR taking to prevent spreading COVID-19?</h4></div>
+  <div><h5>What precautions are DSR taking to prevent spreading COVID-19?</h5></div>
 
   <div>We are following guidelines put in place by the federal government, local governments, and the CDC. We are taking extra safety measures, using hand sanitizer throughout the move, and even practicing social distancing with our teams and customers.  </div>
 
 
-  <div><h4>Will I be penalized if I cancel or reschedule my move?</h4></div>
+  <div><h5>What if I prefer to cancel or reschedule my move?</h5></div>
 
-  <div>No.  We completely understand if our customers feel more comfortable canceling or postponing their move until this blows over.  If you want to reschedule, we will do everything that we can to find a date that works better for you.</div>
+  <div> We completely understand if our customers feel more comfortable canceling or postponing their move until this blows over.  If you want to reschedule, we will do everything that we can to find a date that works better for you.</div>
 
 
-  <div><h4>Is DSR still hiring team members at this time?</h4></div>
+  <div><h5>Is DSR still hiring team members at this time?</h5></div>
 
   <div>Yes, DSR Moving Corporation is always looking for movers and drivers. </div>
   
 
 </div> 
-
 <!-- footer -->
 <?php include 'footer.html';?>
 
