c537aefa3d55ceec9f869468483a083c82f09827
LaMana Donadelle <36721091+lamanaml@users.noreply.github.com>
2020-03-20 20:43:11 -0400

    updated css

--- movingguide.php
+++ movingguide.php
@@ -19,83 +19,76 @@
 
 
 <?php include 'nav.html';?>
+  <nav class="white pad nav" role="navigation">
+      <a id="logo-container" href="index.php" class="brand-logo "><img src="assets/images/logos/DSRMovingLogo2.jpg" width="60%"></a>
+      <ul class="right hide-on-med-and-down">
+        <li><a href="https://ant.hellomoving.com/DSRMOVING/index.htm">Quote Request</a></li>
+        <li><a href="contact.php">Contact</a></li>
+        <li><a href="careers.php">Careers</a></li>
+        <!--  <li><a href="services.php">Services</a></li>-->
+      </ul> 
+
+  <!-- mobile navigation -->
+    <a href="#" data-target="nav-mobile" class="sidenav-trigger"><i class="material-icons">menu</i></a>
+    <ul id="nav-mobile" class="sidenav">
+      <!-- <li><a href="services.php">Services</a></li> -->
+      <li><a href="contact.php">Contact</a></li>
+      <li><a href="careers.php">Careers</a></li>
+      <li><a href="https://ant.hellomoving.com/DSRMOVING/index.htm">Quote Request</a></li>
+      </ul>
+    </div>
+  </nav>
+</div>
+
 <!-- page title -->
   <div class="page-title-container" >   
-   Moving Guide
+   DSR Moving Guide
   </div>
 
 <!-- <Main Content> -->
 
-
-
-
 <div class="container">
- <div class="divider"></div>
+
   <div class="section">
-    <h5>DSR Moving Guide</h5>
-   
-   <h3 class="white-text">DSR Moving Professionals</h3>
-          <p class="grey-text text-lighten-4">We work closely with our clients to customize the services we provide based on their specific needs and circumstances. We are ready and willing to go the extra mile whenever and wherever we’re needed. Contact us today to get in touch with our team of experienced professionals.</p>
-        </div>
-  </div>
-
-  <div class="divider"></div>
-  <div class="section">
-    <h5>Service Area </h5>
-    <p>We move to and from The Triangle Area (Raleigh, Durham, Chapel Hill) and the Triad (Greensboro, Winston Salem, High Point) and surrounding areas.</p>
-  </div>
-  <div class="divider"></div>
-  <div class="section">
-    <h5>Commercial Moves and Corporate Relocation </h5>
-    <p>Local moves are charged by in 15 minute increments with no minimum, plus a travel charge which includes the cost to get the truck to your location, dollies, shrink wrap, padding, and all of the tools to make you move safe and efficient. We will disassemble things like beds and dressers and reassemble them at the destination.</p>
-  </div>
-
-      <div class="divider"></div>
-  <div class="section">
-    <h5>Paying for your Move</h5>
-    <p>We accept Cash, Credit Cards and Checks.  We do not require a deposit.   </p>
-  </div>
-
-
- 
-    <div class="divider"></div>
-  <div class="section">
-    <h5>Add on Services</h5>
-    <p>  Why choose us?
-We move to and from The Triangle Area (Raleigh, Durham, Chapel Hill) and the Triad (Greensboro, Winston Salem, High Point) and surrounding areas.
-
-​
-
-We are licensed and insured
-
-MC NC 625065  
-
-DOT 1702577    
-
-C-2515
-
-We can accommodate same day and next day moves 
-
-
-We move on Sundays
-
-We do not increase our prices for last minute moves or for weekend moves
-
-Our movers are experienced professionals
-
-We offer local and long distance moving services for individuals, companies, and employee relocation
-
-
-
-Load/Unload only (Pods, ABF, Uhaul...)
-
-
-Contact us to get your move started.</p>
-  </div>
+      <p >We work closely with our clients to customize the services we provide based on their specific needs and circumstances. We are ready and willing to go the extra mile whenever and wherever we’re needed. Contact us today to get in touch with our team of experienced professionals.</p>
     </div>
 
+    <div class="divider"></div>
+  
+    <div class="section">
+      <h5>Service Area </h5>
+      <p>We move to and from The Triangle Area (Raleigh, Durham, Chapel Hill) and the Triad (Greensboro, Winston Salem, High Point) and surrounding areas.</p>
+    </div>
+    
+    <div class="divider"></div>
+  
+    <div class="section">
+      <h5>Commercial Moves and Corporate Relocation </h5>
+      <p>Local moves are charged by in 15 minute increments with no minimum, plus a travel charge which includes the cost to get the truck to your location, dollies, shrink wrap, padding, and all of the tools to make you move safe and efficient. We will disassemble things like beds and dressers and reassemble them at the destination.</p>
+    </div>
 
+   <div class="divider"></div>
+    <div class="section">
+      <h5>Paying for your Move</h5>
+      <p>We accept Cash, Credit Cards and Checks.  We do not require a deposit.   </p>
+    </div>
 
+    <div class="divider"></div>
+    <div class="section">
+      <h5>Why Choose Us</h5>
+             <ul>
+        <li> We move to and from The Triangle Area (Raleigh, Durham, Chapel Hill) and the Triad (Greensboro, Winston Salem, High Point) and surrounding areas.</li>
+          <li> We are licensed and insured</li>
+          <li>We can accommodate same day and next day moves </li>
+          <li>We move on Sundays</li>
+          <li>We do not increase our prices for last minute moves or for weekend moves</li>
+          <li>Our movers are experienced professionals</li>
+          <li>We offer local and long distance moving services for individuals, companies, and employee relocation</li>
+          <li>We offer labor only moves - Load/Unload only (Pods, ABF, Uhaul...)</li>
+      </ul>
+    </div>
+    <p>Contact us to get your move started.</p>
+  </div>
 
 
 <!-- footer -->
